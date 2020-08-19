9be7e7c4cd234ddaa950e01c8041a3a9265970f1
WINDOWS <WINDOWS@DESKTOP-KJSBVNF>
2020-08-19 13:39:26 +0530

    successfully created

---
+++ Flipkart/src/test/java/com/flipkart/search/SearchProduct.java
@@ -0,0 +1,12 @@
+package com.flipkart.search;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SearchProduct {
+@Test
+	public void searchproducttest() {
+		 
+	Reporter.log("Search product is working successfully",true);
+	}
+}
